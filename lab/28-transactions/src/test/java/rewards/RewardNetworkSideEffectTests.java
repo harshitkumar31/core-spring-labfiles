a36cc1ac9883eec4cdafd875736e4e071acb23fc
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2024-11-02 22:30:10 +0530

    Transactions lab

--- lab/28-transactions/src/test/java/rewards/RewardNetworkSideEffectTests.java
+++ lab/28-transactions/src/test/java/rewards/RewardNetworkSideEffectTests.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.springframework.transaction.annotation.Transactional;
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,6 +35,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { SystemTestConfig.class })
 @TestMethodOrder(MethodOrderer.MethodName.class)
+@Transactional
 public class RewardNetworkSideEffectTests {
 
 	private static final String SAVINGS_SQL = "select SAVINGS from T_ACCOUNT_BENEFICIARY where NAME = ?";
